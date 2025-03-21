5f6f4baac739d583f4e59dc2d0a049564cb645b7
paubarrio <spam.is.not.friendly@gmail.com>
2025-03-21 00:26:06 -0400

    1226march21

--- quiz.js
+++ quiz.js
@@ -1,19 +1,64 @@
 const quizData = [
     {
         question: "What type of radiation is used in a CT scan for diagnosing SCLC?",
-        options: ["Gamma rays", "X-rays", "Ultraviolet radiation", "Non-ionizing radiation"],
+        options: [
+            "Gamma rays",
+            "X-rays",
+            "Ultraviolet radiation",
+            "Non-ionizing radiation"
+        ],
         correct: 1
     },
     {
         question: "Which of the following describes a key characteristic of ionizing radiation?",
-        options: ["Emits visible light", "Releases a small amount of localized energy", "Causes orbital electrons to be ejected", " Only affects surface tissues"],
+        options: [
+            "Emits visible light",
+            "Releases a small amount of localized energy",
+            "Causes orbital electrons to be ejected",
+            "Only affects surface tissues"
+        ],
         correct: 2
     },
     {
         question: "Which particle type in radiation therapy is best for superficial tumors?",
-        options: ["Photons", "Protons", "Electrons", "Neutrons"],
+        options: [
+            "Photons",
+            "Protons",
+            "Electrons",
+            "Neutrons"
+        ],
         correct: 2
-    }
+    },
+    {
+        question: "Which of the following fractionation schedules is often used for Limited-Stage SCLC?",
+        options: [
+          "45 Gy in 30 fractions, given twice daily (1.5 Gy per fraction)",
+          "70 Gy in 7 fractions (10 Gy per fraction)",
+          "20 Gy in 5 fractions (4 Gy per fraction)",
+          "10 Gy in 2 fractions (5 Gy per fraction)"
+        ],
+        correct: 0
+    },
+    {
+        question: "Which statement best reflects the approximate radiation dose from a single chest CT scan for diagnosing SCLC, according to Larke et al. (2011)?",
+        options: [
+            "Men and women receive the same dose (~2.1 mSv)",
+            "Women receive a higher dose (~2.1 mSv) compared to men (~1.6 mSv)",
+            "Women receive half the dose that men do (~0.8 mSv vs. 1.6 mSv)",
+            "Both men and women receive only about 0.5 mSv"
+     ],
+        correct: 1
+    },
+    {
+        question: "What is the primary aim of Prophylactic Cranial Irradiation (PCI) in SCLC?",
+        options: [
+            "To reduce the risk of severe esophagitis",
+            "To reduce the risk of brain metastases",
+            "To improve local control in the thorax",
+            "To treat asymptomatic pleural effusions"
+        ],
+        correct: 1
+    },
 ];
 
 let currentQuestion = 0;
